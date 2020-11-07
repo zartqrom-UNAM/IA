eaf00c1d777a2e00b924683b76f06cd0ab4b22aa
Zart Qrom <zart_qrom@outlook.com>
2020-11-06 22:17:46 -0600

    Se agrego una sentencia if para verificar el tipo del nodo auxiliar para su posterior tratamiento

--- IA/Proyecto 1/TSP_BFS.py
+++ IA/Proyecto 1/TSP_BFS.py
@@ -1,2 +1,12 @@
 def verificar_nodo_final(nodo_actual, nodo_final):
     nodo_actual_aux = nodo_actual #{key:value}
+    if type(nodo_actual_aux) == dict:
+        dato_aux = list(nodo_actual_aux.keys())
+        if dato_aux[0] = nodo_final:
+            #get solo retorna el valor de la llave ingresada
+            dato_busqueda = nodo_actual_aux.get(nodo_final)
+    else:
+        if nodo_actual_aux == nodo_final:
+            print("Estas en el origen")
+            return 1
+    return 0
