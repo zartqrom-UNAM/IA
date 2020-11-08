7ce8d322f045ff8a59fdb05e0cbc904f06642287
Zart Qrom <zart_qrom@outlook.com>
2020-11-08 08:22:47 -0600

    Se cambio la lista que almacena las ciudades por un diccionario de diccionarios

--- IA/Proyecto 1/TSP_BFS.py
+++ IA/Proyecto 1/TSP_BFS.py
@@ -1,26 +1,26 @@
 #Diccionarios de cada ciudad con sus hijos y respectivos costos
-Sib = {'Fag':99, 'Rim':80, 'Ara':140, 'Ora':151}
-Ora = {'Sib':151, 'Zer':71}
-Zer = {'Ora':71, 'Ara':75}
-Ara = {'Sib':140, 'Zer':75, 'Tim':118}
-Tim = {'Ara':118, 'Lug':111}
-Lug = {'Tim':111, 'Meh':70}
-Meh = {'Lug':70, 'Dro':75}
-Dro = {'Meh':75, 'Cra':120}
-Cra = {'Dro': 120, 'Pit':138, 'Rim':146}
-Rim = {'Sib':80, 'Pit':97, 'Cra':146}
-Pit = {'Bu':101, 'Rim':97, 'Cra':138}
-Fag = {'Sib':99, 'Bu':211}
-Bu = {'Fag':211, 'Pit':101, 'Giu':90, 'Urz':85}
-Giu = {'Buc':90}
-Urz = {'Bu':85, 'Hir':98, 'Vas':142}
-Hir = {'Urz':98, 'Efo':86}
-Efo = {'Hir':86}
-Vas = {'Urz':142, 'Ia':92}
-Ia = {'Vas':92, 'Nea':87}
-Nea = {'Ia':87}
-#Lista que almacena los diccionarios de cada ciudad
-cities = [Sib, Ora, Zer, Ara, Tim, Lug, Meh, Dro, Cra, Rim, Pit, Fag, Bu, Giu, Urz, Hir, Efo, Vas, Ia, Nea]
+cities = {
+    'Sib' : {'Fag':99, 'Rim':80, 'Ara':140, 'Ora':151},
+    'Ora' : {'Sib':151, 'Zer':71},
+    'Zer' : {'Ora':71, 'Ara':75},
+    'Ara' : {'Sib':140, 'Zer':75, 'Tim':118},
+    'Tim' : {'Ara':118, 'Lug':111},
+    'Lug' : {'Tim':111, 'Meh':70},
+    'Meh' : {'Lug':70, 'Dro':75},
+    'Dro' : {'Meh':75, 'Cra':120},
+    'Cra' : {'Dro': 120, 'Pit':138, 'Rim':146},
+    'Rim' : {'Sib':80, 'Pit':97, 'Cra':146},
+    'Pit' : {'Bu':101, 'Rim':97, 'Cra':138},
+    'Fag' : {'Sib':99, 'Bu':211},
+    'Bu' : {'Fag':211, 'Pit':101, 'Giu':90, 'Urz':85},
+    'Giu' : {'Buc':90},
+    'Urz' : {'Bu':85, 'Hir':98, 'Vas':142},
+    'Hir' : {'Urz':98, 'Efo':86},
+    'Efo' : {'Hir':86},
+    'Vas' : {'Urz':142, 'Ia':92},
+    'Ia' : {'Vas':92, 'Nea':87},
+    'Nea' : {'Ia':87}
+}
 #Diccionario que almacena las ciudades y sus correspondientes numeros de hijos
 ciudades = {'Sib':4, 'Ora': 2, 'Ara':3, 'Zer':2, 'Tim':2, 'Lug':2, 'Meh':2, 'Dro': 2, 
     'Cra':3, 'Rim':3, 'Pit':3, 'Fag':2, 'Bu':4, 'Giu':1, 'Urz':3, 'Hir':2, 'Efo':1, 'Vas':2, 'Ia':2, 'Nea':1}
@@ -128,5 +128,7 @@ def agregar_visitados(flag_nodo_inicial, nodo_actual):
 def agregar_hijos(nodo_actual):
     hijo = nodo_actual[0]
     print('Hijo: ', hijo)
-    for key in 
+
 #bfs(nodo_inicial, nodo_final)
+
+agregar_hijos(['Fag','Bu'])
